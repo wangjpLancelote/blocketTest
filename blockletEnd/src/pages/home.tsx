255a4d471f9c056fabb0e06dee0ef889658d4482
Lorenzo <wangjp@szsic.cn>
2024-06-27 22:39:14 +0800

    FormBox

--- blockletEnd/src/pages/home.tsx
+++ blockletEnd/src/pages/home.tsx
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom';
 
 import logo from '../logo.svg';
+import CardFormBox from '../components/Form';
 
 function Home() {
   return (
     <header className="app-header">
       <img src={logo} className="app-logo" alt="logo" />
-      <pre style={{ textAlign: 'left' }}>
+      {/* <pre style={{ textAlign: 'left' }}>
         <code>window.blocklet = {JSON.stringify(window.blocklet, null, 2)}</code>
-      </pre>
-      <Link className="app-link" to="/about">
+        
+      </pre> */}
+      <CardFormBox/>
+      {/* <Link className="app-link" to="/about">
         About
       </Link>
       <a className="app-link" href="https://developer.blocklet.io/docs/" target="_blank" rel="noopener noreferrer">
         Learn Blocklet
-      </a>
+      </a> */}
     </header>
   );
 }
