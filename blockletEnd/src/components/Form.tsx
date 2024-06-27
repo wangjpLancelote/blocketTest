47a4e2a213e27b26767afc046f8e25a5d1df3443
Lorenzo <wangjp@szsic.cn>
2024-06-27 22:53:45 +0800

    fix

--- blockletEnd/src/components/Form.tsx
+++ blockletEnd/src/components/Form.tsx
@@ -3,35 +3,95 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
+import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
+import { useForm, Controller } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
+
+interface FormData {
+  userName: string,
+  email: string,
+  phone: number
+}
 
 
-const CardFormBox: React.FC<{}> = (props) => {
+const CardFormBox: React.FC<Partial<FormData>> = (props: Partial<FormData>) => {
+  const { handleSubmit, control, formState: { errors } } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <>
-      <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
+    <Box component={"form"} onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
+      <Card sx={{ maxWidth: 768 }}>
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
+        <Controller
+        name="firstName"
+        control={control}
+        defaultValue=""
+        rules={{ required: 'First name is required' }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label="First Name"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            error={!!errors.firstName}
+            helperText={errors.firstName ? (errors.firstName.message as string) : ''}
+          />
+        )}
+      />
+      <Controller
+        name="lastName"
+        control={control}
+        defaultValue=""
+        rules={{ required: 'Last name is required' }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label="Last Name"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            error={!!errors.lastName}
+            helperText={errors.lastName ? (errors.lastName.message as string) : ''}
+          />
+        )}
+      />
+      <Controller
+        name="email"
+        control={control}
+        defaultValue=""
+        rules={{
+          required: 'Email is required',
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+            message: 'Invalid email address'
+          }
+        }}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            error={!!errors.email}
+            helperText={errors.email ? (errors.email.message as string) : ''}
+          />
+        )}
+      />
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+      <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button size="small">Submit</Button>
+        </Box>
       </CardActions>
     </Card>
-    </>
+    </Box>
   )
 }
 
